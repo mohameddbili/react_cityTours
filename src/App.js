cdf0a9029dddc13f58037e6d3e2174860c3a8971
mohamed-tahar <mohamedtahar.dbili@gmail.com>
2021-04-06 15:01:24 -0400

    react-city tours

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import React, { Component } from 'react';
+
+
+import TourList from './components/TourList';
+class App extends Component {
+  render() {
+    return (
+      <main>
+      <Navbar/>
+      <TourList/>
+      </main>
+    );
+  }
+}
+
+export default App;
